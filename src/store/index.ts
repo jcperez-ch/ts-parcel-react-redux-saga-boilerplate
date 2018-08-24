87e3f8a84f52e6ec6e6c7011cda1241bbdec6213
JC Perez Chavez <jc.perezchavez@gmail.com>
2018-08-24 00:31:39 -0400

    :construction: Fix tslint issues

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,5 @@
-import { applyMiddleware, compose, createStore, Store } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import reduxSaga from 'redux-saga';
-import { ReduxState } from './types';
 
 import reducer from './reducer';
 import saga from './saga';
--- src/store/ready/reducer.ts
+++ src/store/ready/reducer.ts
@@ -1,6 +1,5 @@
 import { AnyAction, Reducer } from 'redux';
 import { APPLICATION_READY } from './action.types';
-import { ReduxState } from '../types';
 
 export const reducer: Reducer<boolean, AnyAction> = (state = false, { type }: AnyAction): boolean => (
     state || type === APPLICATION_READY
